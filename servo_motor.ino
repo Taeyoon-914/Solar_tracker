8145373405a8a98abdee3e1ba6383e180c863bd3
Taeyoon-914 <117727573+Taeyoon-914@users.noreply.github.com>
2022-11-08 20:41:52 +0900

    Add files via upload

---
+++ servo_motor.ino
@@ -0,0 +1,27 @@
+#include <Servo.h>
+int servoPin = 9; 
+
+Servo servo; 
+
+int angle = 0; // servo position in degrees 
+
+void setup() 
+{ 
+    servo.attach(servoPin); 
+} 
+
+
+void loop() 
+{ 
+  for(angle = 0; angle < 180; angle++) 
+  { 
+    servo.write(angle); 
+    delay(0); 
+  } 
+  // now scan back from 180 to 0 degrees
+  for(angle = 180; angle > 0; angle--) 
+  { 
+    servo.write(angle); 
+    delay(0); 
+  } 
+} 
